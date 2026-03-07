540af3eb747f460c0a02f61788d1f1912c835229
AmeenFadel <ameenfadel820@gmail.com>
2026-03-07 04:14:19 +0300

    my portfolio

--- src/assets/index.js
+++ src/assets/index.js
@@ -8,18 +8,14 @@ import menu from "./menu.svg";
 import close from "./close.svg";
 
 import css from "./tech/css.png";
-import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
 import next from "./tech/next.png";
 import bootstrap from "./tech/bootstrap.png";
 import laravel from "./tech/laravel.png";
@@ -27,16 +23,8 @@ import php from "./tech/php.png";
 import sql from "./tech/sql.png";
 import firebase from "./tech/firebase.png";
 
-
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
 import focalX from "./company/focalX.jpg";
 
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
 import EstateinRealEstate from './EstateinRealEstate.jpg'
 import dashboard from './dashboard.jpg'
 import blog from './blog.jpg'
